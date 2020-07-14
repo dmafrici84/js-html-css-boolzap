65ef7f5ab064180e55c1217b287288287edf93c1
Daniele <dmafrici84@gmail.com>
2020-07-14 13:45:44 +0200

    Lc js-html-css-boolzap corretto modificato js ho utilizzato funzioni all'interno di altre funzioni

--- js/script.js
+++ js/script.js
@@ -4,9 +4,23 @@
 // ● Aggiunta di un messaggio : l’utente scrive un testo nella parte bassa e cliccando
 // “invia” il testo viene aggiunto al thread sopra, come messaggio verde
 
+// Milestone 2
+// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina)
+
+// Milestone 3
+// ● Click sul contatto mostra la conversazione del contatto cliccato, è possibile inserire
+// nuovi messaggi per ogni conversazione
+// ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+// permette di cancellare il messaggio selezionato
+
 $(document).ready(init);
 
 // FUNZIONI
+
   function init(){
     addInputEventLisstener();
   }
@@ -14,42 +28,82 @@ $(document).ready(init);
   function addInputEventLisstener() {
 
     var target = $("#messaggio");
+    target.keyup(sendKeyup);
 
-    target.keyup(function(){
+    var button = $(".fa-paper-plane");
+    button.click(sendClick);
 
-      var input = $ (this);
-      var txt = input.val();
+  }
 
-      console.log(txt);
-      console.log(txt.length);
+  function sendKeyup(event) {
+    console.log("hello");
+    var  key = event.which;
+    var input = $ (this);
+    var txt = input.val();
 
-      if (txt.length > 0) {
-        $(".fa-paper-plane").removeClass("non-visibile");
-        $(".fa-microphone").addClass("non-visibile");
-      } else {
-        $(".fa-paper-plane").addClass("non-visibile");
-        $(".fa-microphone").removeClass("non-visibile");
-      }
+    $(".fa-paper-plane").addClass("non-visibile");
+    $(".fa-microphone").removeClass("non-visibile");
 
-    });
+    if (event.which == 13 && txt.length > 0) {
 
-    $(".fa-paper-plane").click(function() {
+      input.val("");
+      sendMessage(txt);
 
-      var txt = target.val();
-      var smsNuovo = $("#nuovoSms");
-      var textSms = $(".templete > .sms");
-      textSms.clone();
-      textSms.children(".sms-proprio").children("p").html(txt);
-      smsNuovo.append(textSms);
+    } else if (txt.length > 0) {
 
-      setTimeout(function() {
-      var textAmico = "ok";
-      var smsNuovoAmico = $("#nuovoSms");
-      var textSmsAmico = $(".templete1 > .sms");
-      textSmsAmico.clone();
-      textSmsAmico.children(".sms-amico").children("p").html(textAmico);
-      smsNuovoAmico.append(textSmsAmico);
-    }, 60000);
-    });
+      $(".fa-paper-plane").removeClass("non-visibile");
+      $(".fa-microphone").addClass("non-visibile");
+
+    }
+
+  }
+
+  function sendClick() {
+
+    $(".fa-paper-plane").addClass("non-visibile");
+    $(".fa-microphone").removeClass("non-visibile");
+
+    var input = $("#messaggio");
+    var txt = input.val();
+
+    if (txt.length > 0) {
+      input.val("");
+      sendMessage(txt);
+    }
+
+  }
+
+  function sendMessage(txt) {
+
+    var smsNuovo = $("#nuovoSms");
+    var textSms = $("#templete > .sms");
+    textSms.clone();
+
+    textSms.children(".sms-proprio").children(".txt-sms").html(txt);
+    textSms.children(".sms-proprio").children(".orario").html(getActualHour());
+
+    smsNuovo.append(textSms);
+
+    setTimeout((sendMessageAmico), 1000);
+  }
+
+  function getActualHour() {
+
+    var date = new Date();
+    return date.getHours() + ":" + date.getMinutes();
+
+  }
+
+  function sendMessageAmico() {
+
+    var textAmico = "ok";
+    var smsNuovoAmico = $("#nuovoSms");
+    var textSmsAmico = $("#templete1 > .sms");
+    textSmsAmico.clone();
+
+    textSmsAmico.children(".sms-amico").children(".txt-sms").html(textAmico);
+    textSmsAmico.children(".sms-amico").children(".orario").html(getActualHour());
+
+    smsNuovoAmico.append(textSmsAmico);
 
   }
